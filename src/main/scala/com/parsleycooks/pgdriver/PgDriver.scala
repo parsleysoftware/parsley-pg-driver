c8bf70069733f883169325c108236d5b63ba9cb1
Asa Zernik <asa.zernik@gmail.com>
2016-06-04 16:57:17 -0700

    Add array support
    Not using any array columns in the app, but I do want to do set
    membership tests without raw string interpolation.

--- src/main/scala/com/parsleycooks/pgdriver/PgDriver.scala
+++ src/main/scala/com/parsleycooks/pgdriver/PgDriver.scala
@@ -1,9 +1,9 @@
 package com.parsleycooks.pgdriver
 
-import com.github.tminglei.slickpg.{ExPostgresDriver, PgDateSupportJoda}
+import com.github.tminglei.slickpg.{ExPostgresDriver, PgArraySupport, PgDateSupportJoda}
 
-trait PgDriver extends ExPostgresDriver with PgDateSupportJoda {
-  override val api = new API with DateTimeImplicits
+trait PgDriver extends ExPostgresDriver with PgDateSupportJoda with PgArraySupport {
+  override val api = new API with DateTimeImplicits with ArrayImplicits
 }
 
 object PgDriver extends PgDriver
