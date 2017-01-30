d498067b691a07f04bc293e7983775aa24465817
Asa Zernik <asa.zernik@gmail.com>
2017-01-30 00:32:18 -0800

    PgDriver: Include codegen-support bits
    Only works for single-level arrays of plain types, but that's good
    enough for now.

--- src/main/scala/com/parsleycooks/pgdriver/PgDriver.scala
+++ src/main/scala/com/parsleycooks/pgdriver/PgDriver.scala
@@ -3,7 +3,7 @@ package com.parsleycooks.pgdriver
 import com.github.tminglei.slickpg.{ExPostgresDriver, PgArraySupport, PgDateSupportJoda}
 
 trait PgDriver extends ExPostgresDriver with PgDateSupportJoda with PgArraySupport {
-  override val api = new API with DateTimeImplicits with ArrayImplicits
+  override val api = new API with DateTimeImplicits with ArrayImplicits with SimpleArrayPlainImplicits
 }
 
 object PgDriver extends PgDriver
