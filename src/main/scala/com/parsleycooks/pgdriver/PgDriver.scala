cb3f65c4197c050cd7f3b76c8d4839d2ff098f9d
Asa Zernik <asa.zernik@gmail.com>
2017-04-24 02:42:59 -0700

    Switch from joda to java.time
    We always install Java 8 anyway, and this has support for Duration

--- src/main/scala/com/parsleycooks/pgdriver/PgDriver.scala
+++ src/main/scala/com/parsleycooks/pgdriver/PgDriver.scala
@@ -1,8 +1,8 @@
 package com.parsleycooks.pgdriver
 
-import com.github.tminglei.slickpg.{ExPostgresDriver, PgArraySupport, PgDateSupportJoda}
+import com.github.tminglei.slickpg.{ExPostgresDriver, PgArraySupport, PgDate2Support}
 
-trait PgDriver extends ExPostgresDriver with PgDateSupportJoda with PgArraySupport {
+trait PgDriver extends ExPostgresDriver with PgDate2Support with PgArraySupport {
   override val api = new API with DateTimeImplicits with ArrayImplicits with SimpleArrayPlainImplicits
 }
 
