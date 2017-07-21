583dd2eab90b1ef5ec183165bdea131d295bae73
Asa Zernik <asa.zernik@gmail.com>
2017-07-20 19:27:40 -0700

    Fix deprecation warning (s/ExPostgresDriver/ExPostgresProfile)

--- src/main/scala/com/parsleycooks/pgdriver/PgDriver.scala
+++ src/main/scala/com/parsleycooks/pgdriver/PgDriver.scala
@@ -1,8 +1,8 @@
 package com.parsleycooks.pgdriver
 
-import com.github.tminglei.slickpg.{ExPostgresDriver, PgArraySupport, PgDate2Support}
+import com.github.tminglei.slickpg.{ExPostgresProfile, PgArraySupport, PgDate2Support}
 
-trait PgDriver extends ExPostgresDriver with PgDate2Support with PgArraySupport {
+trait PgDriver extends ExPostgresProfile with PgDate2Support with PgArraySupport {
   override val api = new API with DateTimeImplicits with ArrayImplicits with SimpleArrayPlainImplicits
 }
 
