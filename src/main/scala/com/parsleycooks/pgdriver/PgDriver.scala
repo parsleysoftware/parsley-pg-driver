238f9638367574062e0285c4792d83d5bf8c30cb
David Arias <fdariasm@gmail.com>
2020-03-10 15:41:07 -0500

    Add hstore support
    We are going to add an hstore column with localized versions of the product's names

--- src/main/scala/com/parsleycooks/pgdriver/PgDriver.scala
+++ src/main/scala/com/parsleycooks/pgdriver/PgDriver.scala
@@ -1,13 +1,14 @@
 package com.parsleycooks.pgdriver
 
-import com.github.tminglei.slickpg.{ExPostgresProfile, PgArraySupport, PgDate2Support, PgEnumSupport}
+import com.github.tminglei.slickpg.{ExPostgresProfile, PgArraySupport, PgDate2Support, PgEnumSupport, PgHStoreSupport}
 
 trait PgDriver extends ExPostgresProfile
     with PgDate2Support
     with PgArraySupport
     with PgEnumSupport
+    with PgHStoreSupport
 {
-  trait PgDriverAPI extends API with DateTimeImplicits with ArrayImplicits with SimpleArrayPlainImplicits
+  trait PgDriverAPI extends API with DateTimeImplicits with ArrayImplicits with SimpleArrayPlainImplicits with HStoreImplicits
   override val api = new PgDriverAPI {}
 }
 
