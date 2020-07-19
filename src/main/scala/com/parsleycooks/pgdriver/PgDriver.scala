3c3de69092371ada391b5b8be1208f9eb9a8730c
Asa Zernik <asa.zernik@gmail.com>
2020-07-18 19:16:06 -0700

    Add codegen support for temporal types
    Hidden in the source for PgDate2Support:
    
      // let user to call this, since we have more than one `TIMETZ, DATETIMETZ, INTERVAL` binding candidates here
      def bindPgDateTypesToScala[DATE, TIME, DATETIME, TIMETZ, DATETIMETZ, INTERVAL](

--- src/main/scala/com/parsleycooks/pgdriver/PgDriver.scala
+++ src/main/scala/com/parsleycooks/pgdriver/PgDriver.scala
@@ -18,6 +18,12 @@ trait PgDriver extends ExPostgresProfile
 
   val pgjson = "jsonb" // we're using new enough PG versions that more efficient jsonb is available
 
+  super[PgDate2Support].bindPgDateTypesToScala[
+    java.time.LocalDate, java.time.LocalTime, java.time.Instant,
+    // OffsetTime isn't quite right, since it's a fixed offset instead of Zone, but PG doesn't support tz sanely anyway
+    java.time.OffsetTime, java.time.Instant, java.time.Duration,
+  ]
+
   override val api = new PgDriverAPI {}
 }
 
